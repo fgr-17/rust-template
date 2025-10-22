41b029006c982e357609a306a31fcb642c5b8475
Federico Roux <rouxfederico@gmail.com>
2025-10-22 09:34:45 -0300

    [ZL-8] feat(tmpl): Adding LCOV reports (#2)
    This PR adds a new target in the Makefile to generate coverage reports.
    
    There are 2 different outputs for this report:
    - Normal lcov html report, can be found in `target/lcov`
    - coverage-metrics summary table, which is tracked to show the diff of
      coverage on each PR

---
+++ hello/coverage.mk
@@ -0,0 +1,21 @@
+COVERAGE_DIR := target/lcov
+RUSTFLAGS := -Cinstrument-coverage
+LLVM_PROFILE_FILE := $(COVERAGE_DIR)/coverage-%p-%m.profraw
+COVERAGE_OUTPUT_FILE := coverage-metrics
+
+export RUSTFLAGS
+export LLVM_PROFILE_FILE
+
+cov: clean-cov
+	cargo test
+	cargo llvm-cov report --html
+	cargo llvm-cov report --summary-only --output-path $(COVERAGE_OUTPUT_FILE)
+
+
+.PHONY: clean-cov
+
+clean-cov:
+	# Remove old profile data and the target directory
+	rm -f *.profraw
+	rm -rf $(COVERAGE_DIR)
+	rm -f $(COVERAGE_OUTPUT_FILE)
